4cf7c56e5ad5aacb69637a005054f16a5143d57d
Jasper Jager <mail@jasperjager.com>
2019-10-16 11:26:19 +0200

    Add kadastrale gemeente, sectie and gemeentecode

--- src/data/sql/brk/select.kadastrale_sectie.sql
+++ src/data/sql/brk/select.kadastrale_sectie.sql
@@ -2,7 +2,7 @@ SELECT
           kad_gemeentecode->>'omschrijving' || sectie             AS identificatie
          ,sectie                                                  AS code
          ,ST_UNION(geometrie)                                     AS geometrie
-         ,kad_gemeentecode->>'omschrijving'                       AS is_onderdeel_van_gemeente
+         ,kad_gemeentecode->>'omschrijving'                       AS is_onderdeel_van_kadastralegemeentecode
          FROM   brk_prep.kadastraal_object
          WHERE index_letter = 'G'
          GROUP BY kad_gemeentecode->>'omschrijving', sectie
