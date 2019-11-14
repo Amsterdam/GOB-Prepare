fe1ee0f8d3a4f8cbecc6a0394648a22666e0c69f
Nick Barykine <nick.barykine@gmail.com>
2019-11-14 16:00:52 +0100

    [GOB-1027] Add comments to clarify GraphQL behavior

--- src/gobprepare/utils/graphql.py
+++ src/gobprepare/utils/graphql.py
@@ -12,11 +12,21 @@ class GraphQL:
 
     @staticmethod
     def item_to_list(item):
-        # Convert to list
-        items = item if isinstance(item, list) else [item]
-        yield from items
+        # Convert to list if it is not a list
+        item = item if isinstance(item, list) else [item]
+        yield from item
 
     def __iter__(self):
+        """Post to GraphQL API and yield items from the stream.
+
+        GraphQL output for looks like this:
+        [b'{"node": {"a": "1", "b": "xx"}}', b'{"node": {"a": "2", "b": "yy"}}']
+
+        Convert each item to JSON and yield it.
+        The output looks like this:
+        {"node": {"a": "1", "b": "xx"}}
+        {"node": {"a": "2", "b": "yy"}}
+        """
         items = post_stream(f'{self.host}{self.endpoint}', {'query': self.query})
         for item in items:
             yield from self.item_to_list(json.loads(item))
