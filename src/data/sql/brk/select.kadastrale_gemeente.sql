d68ae811d4175ad17b62729e93c1bf2ff8452cab
Jaap Koetsier <jaap@chariot-it.nl>
2019-11-20 16:10:53 +0100

    GOB-744 Use only active KOT's to determine sectie, gemeente and gemeentecodes geometries

--- src/data/sql/brk/select.kadastrale_gemeente.sql
+++ src/data/sql/brk/select.kadastrale_gemeente.sql
@@ -3,5 +3,5 @@ SELECT
          ,ST_UNION(geometrie)                                     AS geometrie
          ,brg_gemeente->>'omschrijving'                           AS ligt_in_gemeente
          FROM   brk_prep.kadastraal_object
-         WHERE index_letter = 'G' AND ST_IsValid(geometrie)
+         WHERE index_letter = 'G' AND ST_IsValid(geometrie) AND modification is NULL and status_code <> 'H'
          GROUP BY kad_gemeente->>'omschrijving', brg_gemeente->>'omschrijving'
--- src/data/sql/brk/select.kadastrale_gemeentecode.sql
+++ src/data/sql/brk/select.kadastrale_gemeentecode.sql
@@ -3,5 +3,5 @@ SELECT
          ,ST_UNION(geometrie)                                     AS geometrie
          ,kad_gemeente->>'omschrijving'                           AS is_onderdeel_van_kadastralegemeente
          FROM   brk_prep.kadastraal_object
-         WHERE index_letter = 'G' AND ST_IsValid(geometrie)
+         WHERE index_letter = 'G' AND ST_IsValid(geometrie) AND modification is NULL and status_code <> 'H'
          GROUP BY kad_gemeentecode->>'omschrijving', kad_gemeente->>'omschrijving'
