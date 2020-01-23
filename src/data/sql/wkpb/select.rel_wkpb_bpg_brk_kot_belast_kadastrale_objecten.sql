eb7360470fdd195538ec77402a86308d67e28edb
Jasper Jager <mail@jasperjager.com>
2020-01-23 14:06:53 +0100

    Add wkpb to prepare to import the relation table
    Uses the current import query for rel_wkpb_bpg_brk_kot_belast_kadastrale objecten and joins the kadastrale objecten into a relation table. This can be imported in GOB-Import. Also add an index to kadastrale_aanduiding on the KOT object to speed up the join.

---
+++ src/data/sql/wkpb/select.rel_wkpb_bpg_brk_kot_belast_kadastrale_objecten.sql
@@ -0,0 +1,13 @@
+SELECT
+  bpg.id,
+  bpg.src_id,
+  bpg.src_volgnummer,
+  bpg.bronwaarde,
+  kot.brk_kot_id                AS dst_id,
+  kot.nrn_kot_volgnr            AS dst_volgnummer
+FROM
+  wkpb_prep.beperkingen bpg
+INNER JOIN
+  brk_prepared.kadastraal_object kot
+ON
+  bpg.bronwaarde = kot.kadastrale_aanduiding
