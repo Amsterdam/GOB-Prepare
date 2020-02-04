3dcea5fba99acbf49fc99afd909a5c79c6cbf53d
Jaap Koetsier <jaap@chariot-it.nl>
2020-02-04 13:09:14 +0100

    PGB-1047 Avoid duplicates in WKPB relation import

--- src/data/sql/wkpb/select.rel_wkpb_bpg_brk_kot_belast_kadastrale_objecten.sql
+++ src/data/sql/wkpb/select.rel_wkpb_bpg_brk_kot_belast_kadastrale_objecten.sql
@@ -1,13 +1,10 @@
-SELECT
-  bpg.id,
-  bpg.src_id,
-  bpg.src_volgnummer,
-  bpg.bronwaarde,
-  kot.brk_kot_id                AS dst_id,
-  kot.nrn_kot_volgnr            AS dst_volgnummer
-FROM
-  wkpb_prep.beperkingen bpg
-INNER JOIN
-  brk_prepared.kadastraal_object kot
-ON
-  bpg.bronwaarde = kot.kadastrale_aanduiding
+SELECT bpg.id,
+       bpg.src_id,
+       bpg.src_volgnummer,
+       bpg.bronwaarde,
+       kot.brk_kot_id          AS dst_id,
+       max(kot.nrn_kot_volgnr) AS dst_volgnummer
+FROM wkpb_prep.beperkingen bpg
+INNER JOIN brk_prepared.kadastraal_object kot
+        ON bpg.bronwaarde = kot.kadastrale_aanduiding
+GROUP BY bpg.id, bpg.src_id, bpg.src_volgnummer, bpg.bronwaarde, kot.brk_kot_id
