a356fcbe86b689a3c4d3e0a1ea5133b2178035ae
Jaap Koetsier <jaap@chariot-it.nl>
2020-03-19 12:16:52 +0100

    Prevent duplicate bronwaardes in prepared KOT relations

--- src/data/sql/brk/kot.add_relatie_g_perceel.sql
+++ src/data/sql/brk/kot.add_relatie_g_perceel.sql
@@ -17,32 +17,34 @@ UPDATE brk_prep.kadastraal_object kot
 SET ontstaan_uit_kadastraalobject=q.ontstaan_uit_kadastraalobject
 FROM (
     SELECT
-        kot.nrn_kot_id,
-        kot.nrn_kot_volgnr,
+        nrn_kot_id,
+        nrn_kot_volgnr,
         array_to_json(
-            array_agg(
-                json_build_object(
-                    'brk_kot_id', ontst_uit_kot.brk_kot_id,
-                    'nrn_kot_id', ontst_uit_kot.nrn_kot_id,
-                    'kot_volgnummer', ontst_uit_kot.nrn_kot_volgnr
-                ) ORDER BY
-                    ontst_uit_kot.brk_kot_id,
-                    ontst_uit_kot.nrn_kot_id,
-                    ontst_uit_kot.nrn_kot_volgnr
-            )
-        ) AS ontstaan_uit_kadastraalobject
-    FROM brk_prep.kadastraal_object kot
-    LEFT JOIN brk_prep.zakelijk_recht zrt_o
-        ON zrt_o.rust_op_kadastraalobject_id=kot.nrn_kot_id
-        AND zrt_o.rust_op_kadastraalobj_volgnr=kot.nrn_kot_volgnr
-    LEFT JOIN brk_prep.zakelijk_recht zrt_b
-        ON zrt_b.betrokken_bij_asg_id=zrt_o.ontstaan_uit_asg_id
-    LEFT JOIN brk_prep.kadastraal_object ontst_uit_kot
-        ON zrt_b.rust_op_kadastraalobject_id=ontst_uit_kot.nrn_kot_id
-        AND zrt_b.rust_op_kadastraalobj_volgnr=ontst_uit_kot.nrn_kot_volgnr
-    WHERE kot.index_letter = 'A'
-        AND zrt_o.ontstaan_uit_asg_id IS NOT NULL
-    GROUP BY kot.nrn_kot_id, kot.nrn_kot_volgnr
+                array_agg(
+                        json_build_object(
+                                'brk_kot_id', ontst_uit_brk_kot_id
+                            ) ORDER BY
+                            ontst_uit_brk_kot_id
+                    )
+            ) AS ontstaan_uit_kadastraalobject
+    FROM (
+         SELECT kot.nrn_kot_id,
+                kot.nrn_kot_volgnr,
+                ontst_uit_kot.brk_kot_id AS ontst_uit_brk_kot_id
+         FROM brk_prep.kadastraal_object kot
+                  LEFT JOIN brk_prep.zakelijk_recht zrt_o
+                            ON zrt_o.rust_op_kadastraalobject_id = kot.nrn_kot_id
+                                AND zrt_o.rust_op_kadastraalobj_volgnr = kot.nrn_kot_volgnr
+                  LEFT JOIN brk_prep.zakelijk_recht zrt_b
+                            ON zrt_b.betrokken_bij_asg_id = zrt_o.ontstaan_uit_asg_id
+                  LEFT JOIN brk_prep.kadastraal_object ontst_uit_kot
+                            ON zrt_b.rust_op_kadastraalobject_id = ontst_uit_kot.nrn_kot_id
+                                AND zrt_b.rust_op_kadastraalobj_volgnr = ontst_uit_kot.nrn_kot_volgnr
+         WHERE kot.index_letter = 'A'
+           AND zrt_o.ontstaan_uit_asg_id IS NOT NULL
+         GROUP BY kot.nrn_kot_id, kot.nrn_kot_volgnr, ontst_uit_kot.brk_kot_id
+    ) qq
+    GROUP BY nrn_kot_id, nrn_kot_volgnr
 ) q(nrn_kot_id, nrn_kot_volgnr, ontstaan_uit_kadastraalobject)
 WHERE kot.ontstaan_uit_kadastraalobject = 'null'
     AND kot.index_letter = 'A'
@@ -55,31 +57,34 @@ UPDATE brk_prep.kadastraal_object kot
 SET relatie_g_perceel=q.relatie_g_perceel
 FROM (
     SELECT
-        kot.nrn_kot_id,
-        kot.nrn_kot_volgnr,
+        nrn_kot_id,
+        nrn_kot_volgnr,
         array_to_json(
             array_agg(
                 json_build_object(
-                    'brk_kot_id', ontst_uit_kot.brk_kot_id,
-                    'nrn_kot_id', ontst_uit_kot.nrn_kot_id,
-                    'kot_volgnummer', ontst_uit_kot.nrn_kot_volgnr
+                    'brk_kot_id', ontst_uit_brk_kot_id
                 ) ORDER BY
-                    ontst_uit_kot.brk_kot_id,
-                    ontst_uit_kot.nrn_kot_id,
-                    ontst_uit_kot.nrn_kot_volgnr
+                    ontst_uit_brk_kot_id
             )
         ) AS relatie_g_perceel
-    FROM brk_prep.kadastraal_object kot
-    LEFT JOIN jsonb_array_elements(kot.ontstaan_uit_kadastraalobject) json_elms(obj)
-        ON TRUE
-    LEFT JOIN brk_prep.kadastraal_object ontst_uit_kot
-        ON ontst_uit_kot.nrn_kot_id=(json_elms.obj->>'nrn_kot_id')::integer
-        AND ontst_uit_kot.nrn_kot_volgnr=(json_elms.obj->>'kot_volgnummer')::integer
-    WHERE ontst_uit_kot.index_letter='G'
-        AND kot.index_letter='A'
-        AND kot.relatie_g_perceel = 'null'
-        AND kot.ontstaan_uit_kadastraalobject <> 'null'
-    GROUP BY kot.nrn_kot_id, kot.nrn_kot_volgnr
+    FROM (
+          SELECT
+            kot.nrn_kot_id,
+            kot.nrn_kot_volgnr,
+            ontst_uit_kot.brk_kot_id AS ontst_uit_brk_kot_id
+          FROM brk_prep.kadastraal_object kot
+        LEFT JOIN jsonb_array_elements(kot.ontstaan_uit_kadastraalobject) json_elms(obj)
+            ON TRUE
+        LEFT JOIN brk_prep.kadastraal_object ontst_uit_kot
+            ON ontst_uit_kot.nrn_kot_id=(json_elms.obj->>'nrn_kot_id')::integer
+            AND ontst_uit_kot.nrn_kot_volgnr=(json_elms.obj->>'kot_volgnummer')::integer
+        WHERE ontst_uit_kot.index_letter='G'
+            AND kot.index_letter='A'
+            AND kot.relatie_g_perceel = 'null'
+            AND kot.ontstaan_uit_kadastraalobject <> 'null'
+        GROUP BY kot.nrn_kot_id, kot.nrn_kot_volgnr, ontst_uit_kot.brk_kot_id
+    ) qq
+    GROUP BY nrn_kot_id, nrn_kot_volgnr
 ) q(nrn_kot_id, nrn_kot_volgnr, relatie_g_perceel)
 WHERE kot.nrn_kot_id=q.nrn_kot_id
     AND kot.nrn_kot_volgnr=q.nrn_kot_volgnr
@@ -103,33 +108,34 @@ BEGIN
                 kot.nrn_kot_id,
                 kot.nrn_kot_volgnr,
                 array_to_json(
-                    array_agg(
-                        json_build_object(
-                            'brk_kot_id', gperceel.brk_kot_id,
-                            'nrn_kot_id', gperceel.nrn_kot_id,
-                            'kot_volgnummer', gperceel.nrn_kot_volgnr
-                        ) ORDER BY
-                            gperceel.brk_kot_id,
-                            gperceel.nrn_kot_id,
-                            gperceel.nrn_kot_volgnr
-                    )
-                ) AS relatie_g_perceel
-            FROM brk_prep.kadastraal_object kot
-            LEFT JOIN jsonb_array_elements(kot.ontstaan_uit_kadastraalobject) json_elms(obj)
-                ON TRUE
-            LEFT JOIN brk_prep.kadastraal_object ontst_uit_kot
-                ON ontst_uit_kot.nrn_kot_id=(json_elms.obj->>'nrn_kot_id')::integer
-                AND ontst_uit_kot.nrn_kot_volgnr=(json_elms.obj->>'kot_volgnummer')::integer
-            LEFT JOIN jsonb_array_elements(ontst_uit_kot.relatie_g_perceel) json_gperc_elms(obj)
-                ON TRUE
-            LEFT JOIN brk_prep.kadastraal_object gperceel
-                ON gperceel.nrn_kot_id=(json_gperc_elms.obj->>'nrn_kot_id')::integer
-                AND gperceel.nrn_kot_volgnr=(json_gperc_elms.obj->>'kot_volgnummer')::integer
-            WHERE ontst_uit_kot.relatie_g_perceel <> 'null'
-                AND kot.index_letter = 'A'
-                AND kot.relatie_g_perceel = 'null'
-                AND kot.ontstaan_uit_kadastraalobject <> 'null'
-            GROUP BY kot.nrn_kot_id, kot.nrn_kot_volgnr
+                        array_agg(
+                                json_build_object(
+                                        'brk_kot_id', gperc_brk_kot_id
+                                    ) ORDER BY gperc_brk_kot_id
+                            )
+                    ) AS relatie_g_perceel
+            FROM (
+                 SELECT kot.nrn_kot_id,
+                        kot.nrn_kot_volgnr,
+                        gperceel.brk_kot_id AS gperc_brk_kot_id
+                 FROM brk_prep.kadastraal_object kot
+                          LEFT JOIN jsonb_array_elements(kot.ontstaan_uit_kadastraalobject) json_elms(obj)
+                                    ON TRUE
+                          LEFT JOIN brk_prep.kadastraal_object ontst_uit_kot
+                                    ON ontst_uit_kot.nrn_kot_id = (json_elms.obj ->> 'nrn_kot_id')::integer
+                                        AND ontst_uit_kot.nrn_kot_volgnr = (json_elms.obj ->> 'kot_volgnummer')::integer
+                          LEFT JOIN jsonb_array_elements(ontst_uit_kot.relatie_g_perceel) json_gperc_elms(obj)
+                                    ON TRUE
+                          LEFT JOIN brk_prep.kadastraal_object gperceel
+                                    ON gperceel.nrn_kot_id = (json_gperc_elms.obj ->> 'nrn_kot_id')::integer
+                                        AND gperceel.nrn_kot_volgnr = (json_gperc_elms.obj ->> 'kot_volgnummer')::integer
+                 WHERE ontst_uit_kot.relatie_g_perceel <> 'null'
+                   AND kot.index_letter = 'A'
+                   AND kot.relatie_g_perceel = 'null'
+                   AND kot.ontstaan_uit_kadastraalobject <> 'null'
+                 GROUP BY kot.nrn_kot_id, kot.nrn_kot_volgnr, gperc_brk_kot_id
+            ) qq
+            GROUP BY nrn_kot_id, nrn_kot_volgnr
         ) q(nrn_kot_id, nrn_kot_volgnr, relatie_g_perceel)
         WHERE kot.nrn_kot_id=q.nrn_kot_id
             AND kot.nrn_kot_volgnr=q.nrn_kot_volgnr
