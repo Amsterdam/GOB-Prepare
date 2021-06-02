633e0da415775173009900c1e40b260092a66de8
Roel de Vries <roel.de.vries@amsterdam.nl>
2021-06-02 14:12:15 +0200

    Use SnapToGrid before unioning geometries

--- src/data/sql/brk/select.kadastrale_gemeente.sql
+++ src/data/sql/brk/select.kadastrale_gemeente.sql
@@ -1,7 +1,10 @@
-SELECT
-          kad_gemeente->>'omschrijving'                           AS identificatie
-         ,ST_UNION(geometrie)                                     AS geometrie
-         ,brg_gemeente->>'omschrijving'                           AS ligt_in_gemeente
-         FROM   brk_prep.kadastraal_object
-         WHERE index_letter = 'G' AND ST_IsValid(geometrie) AND modification is NULL and status_code <> 'H'
-         GROUP BY kad_gemeente->>'omschrijving', brg_gemeente->>'omschrijving'
+-- use SnapToGrid to prevent precision errors
+SELECT kad_gemeente ->> 'omschrijving'                      AS identificatie,
+       ST_UNION(ST_SnapToGrid(geometrie, 0.000001))  AS geometrie,
+       brg_gemeente ->> 'omschrijving'                      AS ligt_in_gemeente
+FROM brk_prep.kadastraal_object
+WHERE index_letter = 'G'
+  AND ST_IsValid(geometrie)
+  AND modification is NULL
+  and status_code <> 'H'
+GROUP BY kad_gemeente ->> 'omschrijving', brg_gemeente ->> 'omschrijving'
