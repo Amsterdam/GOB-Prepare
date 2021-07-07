8825b9e77c94bd5e0ba31f9644ba703eef91add1
Roel de Vries <roel.de.vries@amsterdam.nl>
2021-07-07 16:54:58 +0200

    Fix depends_on order for `overlijden`

--- src/data/sql/brp/brp_overlijden.sql
+++ src/data/sql/brp/brp_overlijden.sql
@@ -1,8 +1,6 @@
-
 select bwh.burgerservicenummer as burgerservicenummer,
        bwh.datum_overlijden    as datum_overlijden,
        null                    as plaats_overlijden,
        null                    as land_overlijden
-
 from brp.bewoning_historisch bwh
 where datum_overlijden is not null
