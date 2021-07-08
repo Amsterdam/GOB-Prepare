6ead26a65851cc13499f4638b53e08c55ec5f59c
Michiel <m.kuik@amsterdam.nl>
2021-07-08 13:07:08 +0200

    AddOudersRelation

---
+++ src/data/sql/brp/brp_ouders.sql
@@ -0,0 +1,49 @@
+select prs.systeem_nummer_persoon||'.'||'ouder1'  as identificatie,
+--        prs.ouder1_bsn                          as burgerservicenummer,
+       null                                       as burgerservicenummer,
+--        prs.ouder1_voornamen                    as voornamen,
+       null                                       as voornamen,
+--        prs.ouder1_voorvoegsel                  as voorvoegsels,
+       null                                       as voorvoegsels,
+--        prs.ouder1_geslachtsnaam                as geslachtsnaam,
+       null                                       as geslachtsnaam,
+       prs.geslachtsaanduiding                    as geslachtsaanduiding_code,
+       null                                       as geslachtsaanduiding_oms,
+--        prs.ouder1_geboortedatum                as geboortedatum,
+       null                                       as geboortedatum,
+--         prs.ouder1_geboorteplaats              as geboorteplaats_oms,
+       null                                       as geboorteplaats_oms,
+       null                                       as geboorteplaats_pl_in_buitlnd,
+       null                                       as geboorteplaats_heeft_gem_code,
+--         prs.ouder1_geboorteland                as geboorteland,
+       null                                       as geboorteland
+
+from brp.personen_actueel prs
+where ouder1_geslachtsnaam != '.'
+
+union all
+
+select prs.systeem_nummer_persoon||'.'||'ouder2'  as identificatie,
+--        prs.ouder2_bsn                          as burgerservicenummer,
+       null                                       as burgerservicenummer,
+--        prs.ouder2_voornamen                    as voornamen,
+       null                                       as voornamen,
+--        prs.ouder2_voorvoegsel                  as voorvoegsels,
+       null                                       as voorvoegsels,
+--        prs.ouder2_geslachtsnaam                as geslachtsnaam,
+       null                                       as geslachtsnaam,
+       prs.geslachtsaanduiding                    as geslachtsaanduiding_code,
+       null                                       as geslachtsaanduiding_oms,
+--        prs.ouder2_geboortedatum                as geboortedatum,
+       null                                       as geboortedatum,
+--         prs.ouder2_geboorteplaats              as geboorteplaats_oms,
+       null                                       as geboorteplaats_oms,
+       null                                       as geboorteplaats_pl_in_buitlnd,
+       null                                       as geboorteplaats_heeft_gem_code,
+--         prs.ouder2_geboorteland                as geboorteland,
+       null                                       as geboorteland
+
+from brp.personen_actueel prs
+where ouder2_geslachtsnaam != '.' and ouder2_geslachtsnaam is not null
+
+
