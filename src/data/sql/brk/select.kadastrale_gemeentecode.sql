224b795a3894041725047f516df480614dc57eff
Roel de Vries <roel.de.vries@amsterdam.nl>
2021-07-19 09:54:32 +0200

    Fix the gemeentecode query

--- src/data/sql/brk/select.kadastrale_gemeentecode.sql
+++ src/data/sql/brk/select.kadastrale_gemeentecode.sql
@@ -1,21 +1,33 @@
--- The last union is to create MULTIPOLYGONs from multiple areas with the same identificatie
-select gc2.identificatie,
-       ST_Union(gc2.geometrie) as geometrie,
-       gc2.is_onderdeel_van_kadastralegemeente
+-- Use ST_DumpRings to filter the outer polygon ring and exclude holes
+-- Collect the polygons and Use ST_UnaryUnion to remove conflicting linestrings
+-- Take the exterior ring
+-- The last union is to create MULTIPOLYGONs from areas with the same identificatie
+select gc3.identificatie,
+       ST_Union(gc3.geometrie) as geometrie,
+       gc3.is_onderdeel_van_kadastralegemeente
 from (
-         select gc1.identificatie,
-                ST_MakePolygon(ST_ExteriorRing((ST_Dump(gc1.geometrie)).geom)) as geometrie,
-                gc1.is_onderdeel_van_kadastralegemeente
+         select gc2.identificatie,
+                ST_MakePolygon(ST_ExteriorRing((ST_Dump(gc2.geometrie)).geom)) as geometrie,
+                gc2.is_onderdeel_van_kadastralegemeente
          from (
-                  select kad_gemeentecode ->> 'omschrijving' as identificatie,
-                         ST_Union(ST_UnaryUnion(geometrie))  as geometrie,
-                         kad_gemeente ->> 'omschrijving'     as is_onderdeel_van_kadastralegemeente
-                  from brk_prep.kadastraal_object
-                  where index_letter = 'G'
-                    and ST_IsValid(geometrie)
-                    and modification is NULL
-                    and status_code <> 'H'
-                  group by kad_gemeentecode ->> 'omschrijving', kad_gemeente ->> 'omschrijving'
-              ) gc1
-     ) gc2
-group by gc2.identificatie, gc2.is_onderdeel_van_kadastralegemeente
+                  select gc1.identificatie,
+                         gc1.is_onderdeel_van_kadastralegemeente,
+                         ST_UnaryUnion(ST_Collect(gc1.geometrie)) as geometrie
+                  from (
+                           select kad_gemeentecode ->> 'omschrijving' as identificatie,
+                                  (ST_DumpRings(geometrie)).path      as nrings,
+                                  (ST_DumpRings(geometrie)).geom      as geometrie,
+                                  kad_gemeente ->> 'omschrijving'     as is_onderdeel_van_kadastralegemeente
+                           from brk_prep.kadastraal_object
+                           where index_letter = 'G'
+                             and ST_IsValid(geometrie)
+                             and modification is null
+                             and status_code <> 'H'
+                       ) gc1
+                  where gc1.nrings[1] = 0
+                  group by gc1.identificatie,
+                           gc1.is_onderdeel_van_kadastralegemeente
+              ) gc2
+     ) gc3
+group by gc3.identificatie,
+         gc3.is_onderdeel_van_kadastralegemeente
