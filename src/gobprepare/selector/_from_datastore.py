15f5b24a7ec2e7beaf7f4a933c53db88f3953b2a
Roel de Vries <roel.de.vries@amsterdam.nl>
2021-07-24 01:21:03 +0200

    use named cursor in query

--- src/gobprepare/selector/_from_datastore.py
+++ src/gobprepare/selector/_from_datastore.py
@@ -1,5 +1,10 @@
-class FromDatastoreSelector():
+class FromDatastoreSelector:
     """From Generic Datastore Selector"""
 
+    BATCH_SIZE = 2_000
+
     def _read_rows(self, query):
-        return self._src_datastore.query(query)
+        # kwargs apply to psycopg2 cursor
+        # use named cursor to use server-side-cursor and allow to be used again
+        # arraysize is batchsize of query results
+        yield from self._src_datastore.query(query, name='named_cursor', arraysize=self.BATCH_SIZE, withhold=True)
