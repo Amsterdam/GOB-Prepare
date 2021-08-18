6b10643df4a2f4eefbd6ed189334edab3a4f520c
Michiel <m.kuik@amsterdam.nl>
2021-08-18 16:15:56 +0200

    Add historie data for BRP import

--- src/data/sql/brp/brp_hist_personen.sql
+++ src/data/sql/brp/brp_hist_personen.sql
@@ -1,4 +1,5 @@
 -- personen uit bewoning_historisch die niet actueel zijn
+<<<<<<< refs/remotes/origin/AddHistorieBRP
 select bwh.systeem_nummer_persoon as identificatie
 ,      bwh.burgerservicenummer    as burgerservicenummer
 ,      bwh.geboortedatum          as geboortedatum
@@ -7,3 +8,15 @@ select bwh.systeem_nummer_persoon as identificatie
 from brp.bewoning_historisch bwh
 where bwh.systeem_nummer_persoon not in 
      (select prs.systeem_nummer_persoon from personen_actueel prs)
+=======
+select bwh.systeem_nummer_persoon      as identificatie,
+       bwh.burgerservicenummer         as burgerservicenummer,
+       substr(bwh.geboortedatum, 0, 5) || '-' ||
+       substr(bwh.geboortedatum, 5, 2) || '-' ||
+       substr(bwh.geboortedatum, 7, 2) as geboortedatum,
+       bwh.administratienummer         as a_nummer,
+       bwh.geslachtsaanduiding         as geslachtsaanduiding_code
+from brp.bewoning_historisch bwh
+where bwh.systeem_nummer_persoon not in 
+     (select prs.systeem_nummer_persoon from brp.personen_actueel prs)
+>>>>>>> Add historie data for BRP import
