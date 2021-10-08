78c88e6dc2405f7887fcddce29863de0de46a0ca
Roel de Vries <roel.de.vries@amsterdam.nl>
2021-10-08 17:18:29 +0200

    Adapt queries to new aardaantekening table

--- src/data/sql/brk/select.aantekening_kadastraal_object.sql
+++ src/data/sql/brk/select.aantekening_kadastraal_object.sql
@@ -15,32 +15,36 @@ SELECT atg.identificatie                         AS brk_atg_id
      , LEAST(kot.expiration_date, atg.einddatum) AS expiration_date
 --
 FROM brk.aantekening atg
-         JOIN brk.aantekening_kadastraalobject akt ON (atg.id = akt.aantekening_id)
-         JOIN brk_prep.kadastraal_object kot ON (akt.kadastraalobject_id = kot.nrn_kot_id AND
-                                                 akt.kadastraalobject_volgnummer = kot.nrn_kot_volgnr)
-         LEFT JOIN (
+
+JOIN brk.aantekening_kadastraalobject akt
+    ON atg.id = akt.aantekening_id
+
+JOIN brk_prepared.kadastraal_object kot
+    ON akt.kadastraalobject_id = kot.nrn_kot_id AND akt.kadastraalobject_volgnummer = kot.nrn_kot_volgnr
+
+LEFT JOIN (
     SELECT abn.aantekening_id,
            array_to_json(array_agg(json_build_object('brk_sjt_id', subject_identificatie)
                                    ORDER BY subject_identificatie)) AS brk_sjt_ids
     FROM brk.aantekeningbetrokkenpersoon abn
     GROUP BY abn.aantekening_id
-) abn ON (abn.aantekening_id = atg.id)
-         LEFT JOIN (
+) abn
+    ON abn.aantekening_id = atg.id
+
+LEFT JOIN (
     SELECT geb.aantekening_id,
            array_to_json(array_agg(json_build_object('nrn_sdl_id', sdl.id) ORDER BY sdl.id)) AS nrn_sdl_ids
     FROM brk.aantekeningisgebaseerdop geb
              JOIN brk.stukdeel sdl
                   ON sdl.identificatie = geb.stukdeel_identificatie
     GROUP BY geb.aantekening_id
-) geb ON (geb.aantekening_id = atg.id)
-         JOIN brk.c_aardaantekening aag ON (atg.aardaantekening_code = aag.code)
--- TEMPORARY Filter duplicate identification values (31x brk_atg_id)
-where atg.identificatie not in (
-    select distinct a.brk_atg_id
-    from (
-             select brk_atg_id, count(brk_atg_id)
-             from brk_prep.aantekening_kadastraal_object ako
-             group by brk_atg_id, nrn_kot_volgnr
-             having count(*) > 1
-         ) a
-)
+) geb
+    ON geb.aantekening_id = atg.id
+
+-- Only include aantekeningen which have an aardaantekening_code
+JOIN brk.import_aardaantekening aag
+    ON atg.aardaantekening_code = aag.code
+
+-- Filter all aantekeningen based on aardaantekening != Aantekening PB
+-- https://dev.azure.com/CloudCompetenceCenter/Datateam%20Basis%20en%20Kernregistraties/_workitems/edit/17723
+WHERE aag.type != 'Aantekening PB'
