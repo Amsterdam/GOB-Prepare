70513e0b226ff2f442e4ba718793a089eea81635
Peter Adrichem <Peter.Adrichem@gmail.com>
2022-09-07 17:17:12 +0200

    Run as user datapunt.

--- src/test.sh
+++ src/test.sh
@@ -3,14 +3,14 @@
 set -u # crash on missing env
 set -e # stop on any error
 
-# Clear any cached results
-find . -name "*.pyc" -exec rm -f {} \;
+# Coverage 6: coverage run --data-file=/tmp/.coveragerc …
+export COVERAGE_FILE=/tmp/.coverage
 
 echo "Running style checks"
 flake8
 
 echo "Running unit tests"
-pytest tests/
+coverage run --source=./gobprepare -m pytest tests/
 
-echo "Running coverage tests"
-pytest --cov=gobprepare --cov-report html --cov-fail-under=100
+echo "Coverage report"
+coverage report --show-missing --fail-under=100
