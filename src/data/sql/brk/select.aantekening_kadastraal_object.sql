c841cba31c2141b5ffae26b1b17d342198349ac7
Jeroenriemens <j.riemens@amsterdam.nl>
2022-09-12 17:01:32 +0200

    changed to the BRK2

--- src/data/sql/brk/select.aantekening_kadastraal_object.sql
+++ src/data/sql/brk/select.aantekening_kadastraal_object.sql
@@ -1,40 +1,34 @@
-SELECT atg.identificatie                         AS brk_atg_id
-     , atg.id                                    AS nrn_atg_id
-     , atg.aardaantekening_code                  AS atg_aardaantekening_code
-     , aag.omschrijving                          AS atg_aardaantekening_oms
-     , atg.omschrijving                          AS atg_omschrijving
-     , atg.einddatum                             AS atg_einddatum
-     , kot.brk_kot_id                            AS brk_kot_id     -- BETREKKING OP KOT
-     , kot.nrn_kot_id                            AS nrn_kot_id     -- BETREKKING OP KOT
-     , kot.nrn_kot_volgnr                        AS nrn_kot_volgnr -- BETREKKING OP KOT
-     , abn.brk_sjt_ids                           AS brk_sjt_ids
-     , geb.nrn_sdl_ids                           AS nrn_sdl_ids
-     , kot.toestandsdatum                        AS toestandsdatum
-     , kot.creation                              AS begindatum
-     , kot.einddatum                             AS einddatum
-     , LEAST(kot.expiration_date, atg.einddatum) AS expiration_date
---
-FROM brk.aantekening atg
-         JOIN brk.aantekening_kadastraalobject akt ON (atg.id = akt.aantekening_id)
-         JOIN brk_prep.kadastraal_object kot ON (akt.kadastraalobject_id = kot.nrn_kot_id AND
-                                                 akt.kadastraalobject_volgnummer = kot.nrn_kot_volgnr)
-         LEFT JOIN (
-    SELECT abn.aantekening_id,
-           array_to_json(array_agg(json_build_object('brk_sjt_id', subject_identificatie)
-                                   ORDER BY subject_identificatie)) AS brk_sjt_ids
-    FROM brk.aantekeningbetrokkenpersoon abn
-    GROUP BY abn.aantekening_id
-) abn ON (abn.aantekening_id = atg.id)
-         LEFT JOIN (
-    SELECT geb.aantekening_id,
-           array_to_json(array_agg(json_build_object('nrn_sdl_id', sdl.id) ORDER BY sdl.id)) AS nrn_sdl_ids
-    FROM brk.aantekeningisgebaseerdop geb
-             JOIN brk.stukdeel sdl
-                  ON sdl.identificatie = geb.stukdeel_identificatie
-    GROUP BY geb.aantekening_id
-) geb ON (geb.aantekening_id = atg.id)
-         JOIN brk.c_aardaantekening aag ON (atg.aardaantekening_code = aag.code)
--- TEMPORARY Filter duplicate identification values (31x brk_atg_id)
+SELECT atg.identificatie											AS brk_atg_id
+,atg.id  															AS nrn_atg_id
+,atg.aardaantekening_code 											AS atg_aardaantekening_code
+,aag.omschrijving													AS atg_aardaantekening_oms
+,atg.omschrijving													AS atg_omschrijving
+,atg.einddatum   							                        AS atg_einddatum
+,atg.einddatum_recht												AS atg_eindatum_recht					--jr, 2022-09-09: nieuw 
+,ko.identificatie 													AS brk_kot_id
+,koa.kadastraalobject_id 											AS nrn_kot_id
+,koa.kadastraalobject_volgnummer  									AS nrn_kot_volgnr
+,ko.toestandsdatum    												AS toestandsdatum
+,ko.begin_geldigheid												AS begindatum
+,ko.eind_geldigheid                             					AS einddatum
+,LEAST(ko."_expiration_date" , atg.einddatum, atg.einddatum_recht) 	AS expiration_date
+,atg.betreft_gedeelte_van_perceel									AS atg_betreft_gedeelte_van_perceel		--jr, 2022-09-09: nieuw 
+,atg.stukdeel_identificatie											AS atg_stukdeel_identificatie			--jr, 2022-09-09: nieuw, was multi (nrn_sdl_ids), is nu enkel geworden. kan ook leeg zijn. Vreemd.
+,abn.brk_sjt_ids													AS brk_sjt_ids
+--   geb.nrn_sdl_ids                           AS nrn_sdl_ids 												--jr, 2022-09-12: vervallen, nu enkel (atg_stukdeel_identificatie)
+FROM brk2_jeroen_speeltuin.aantekening atg
+	LEFT JOIN brk2_jeroen_speeltuin.aantekening_betrokkenpersoon abp ON abp.aantekening_id =atg.id 
+	JOIN brk2_jeroen_speeltuin.kadastraal_object_aantekening koa ON koa.aantekening_identificatie =atg.identificatie 
+	LEFT JOIN c_aardaantekening aag ON atg.aardaantekening_code =aag.code
+	LEFT JOIN brk2_prepared.kadastraal_object ko ON (ko.id  =koa.kadastraalobject_id AND ko.volgnummer =koa.kadastraalobject_volgnummer)
+    LEFT JOIN (
+		SELECT apn.aantekening_id,
+		array_to_json(array_agg(json_build_object('brk_sjt_id', subject_identificatie)
+		ORDER BY subject_identificatie)) AS brk_sjt_ids
+		FROM brk2_jeroen_speeltuin.aantekening_betrokkenpersoon apn
+		GROUP BY apn.aantekening_id
+		) abn ON (abn.aantekening_id = atg.id)	
+/*-- TEMPORARY Filter duplicate identification values (31x brk_atg_id)
 where atg.identificatie not in (
     select distinct a.brk_atg_id
     from (
@@ -43,4 +37,4 @@ where atg.identificatie not in (
              group by brk_atg_id, nrn_kot_volgnr
              having count(*) > 1
          ) a
-)
+)*/ --jr, 2022-09-09: hopelijk niet meer nodig, nog niet gechecked
