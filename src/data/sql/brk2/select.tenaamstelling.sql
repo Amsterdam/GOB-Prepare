3b80deb40624b99a2a79ebd2b3273483e853b06d
Jaap Koetsier <jaap@chariot-it.nl>
2022-09-28 09:41:28 +0200

    Rename key in JSONB for stukdeel_identificatie

--- src/data/sql/brk2/select.tenaamstelling.sql
+++ src/data/sql/brk2/select.tenaamstelling.sql
@@ -33,7 +33,7 @@ FROM brk2.tenaamstelling tng
          LEFT JOIN brk2.c_burgerlijkestaat b ON b.code = tng.burgerlijkestaat_code
          LEFT JOIN (SELECT g.tenaamstelling_id,
                            JSON_AGG(JSONB_BUILD_OBJECT(
-                                   g.stukdeel_identificatie, stukdeel_identificatie
+                                   'bronwaarde', stukdeel_identificatie
                                )) isgebaseerd_op
                     FROM brk2.tenaamstelling_isgebaseerdop g
                     GROUP BY g.tenaamstelling_id) g ON tng.id = g.tenaamstelling_id
