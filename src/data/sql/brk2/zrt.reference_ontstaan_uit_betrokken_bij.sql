172f8586c943fa98a02ea6657272c290981988ff
Peter Adrichem <Peter.Adrichem@gmail.com>
2022-12-23 14:30:41 +0100

    BRK2 ZRT *_brk_zakelijke_rechten tabellen. (#398)

--- src/data/sql/brk2/zrt.reference_ontstaan_uit_betrokken_bij.sql
+++ src/data/sql/brk2/zrt.reference_ontstaan_uit_betrokken_bij.sql
@@ -1,10 +1,10 @@
--- Reference betrokken_bij_zakelijkerechten and determine __max_betrokken_bij_begindatum
+-- Reference betrokken_bij_brk_zakelijke_rechten and determine __max_betrokken_bij_begindatum
 UPDATE brk2_prep.zakelijk_recht zrt
-SET betrokken_bij_zakelijkerechten=q2.betrokken_bij_zakelijkerechten,
+SET betrokken_bij_brk_zakelijke_rechten=q2.betrokken_bij_brk_zakelijke_rechten,
     __max_betrokken_bij_begindatum=q2.__max_betrokken_bij_begindatum
 FROM (
     SELECT
-             json_agg(json_build_object('zrt_identificatie', identificatie) ORDER BY identificatie) betrokken_bij_zakelijkerechten,
+             json_agg(json_build_object('zrt_identificatie', identificatie) ORDER BY identificatie) betrokken_bij_brk_zakelijke_rechten,
              max(zrt_begindatum) __max_betrokken_bij_begindatum,
              __ontstaan_uit_asg_id
          FROM (
@@ -18,13 +18,14 @@ FROM (
 ) q2
 WHERE q2.__ontstaan_uit_asg_id = zrt.__betrokken_bij_asg_id;
 
--- Reference ontstaan_uit_zakelijkerechten and determine __max_ontstaan_uit_begindatum
+-- Reference ontstaan_uit_brk_zakelijke_rechten and determine __max_ontstaan_uit_begindatum
 UPDATE brk2_prep.zakelijk_recht zrt
-SET ontstaan_uit_zakelijkerechten=q2.ontstaan_uit_zakelijkerechten,
+SET ontstaan_uit_brk_zakelijke_rechten=q2.ontstaan_uit_brk_zakelijke_rechten,
     __max_ontstaan_uit_begindatum=q2.__max_ontstaan_uit_begindatum
 FROM (
     SELECT
-             json_agg(json_build_object('zrt_identificatie', identificatie) ORDER BY identificatie) ontstaan_uit_zakelijkerechten,
+             json_agg(
+                  json_build_object('zrt_identificatie', identificatie) ORDER BY identificatie) ontstaan_uit_brk_zakelijke_rechten,
              max(zrt_begindatum) __max_ontstaan_uit_begindatum,
              __betrokken_bij_asg_id
          FROM (
