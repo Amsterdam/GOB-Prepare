73655c5c1fe3a2b7461dc6753cb608a01ebff10f
JKoetsier <jaap@chariot-it.nl>
2023-01-31 16:11:42 +0100

    Fix BRK1/BRK2 preselect.zrt_kot_wip queries: avoid error on UPDATE query using Citus (#410)

--- src/data/sql/brk/preselect.zrt_kot_wip.sql
+++ src/data/sql/brk/preselect.zrt_kot_wip.sql
@@ -13,7 +13,8 @@ SELECT zrt.identificatie,
        kot.modification                 AS modification
 FROM brk.zakelijkrecht zrt
          LEFT JOIN brk_prep.kadastraal_object kot
-                   ON zrt.rust_op_kadastraalobject_id = kot.nrn_kot_id AND zrt.rust_op_kadastraalobj_volgnr = kot.nrn_kot_volgnr;
+                   ON zrt.rust_op_kadastraalobject_id = kot.nrn_kot_id AND
+                      zrt.rust_op_kadastraalobj_volgnr = kot.nrn_kot_volgnr;
 
 -- zakelijk_recht objects are linked through the zakelijkrecht_isbelastmet table.
 --
@@ -29,7 +30,45 @@ DECLARE
     max_iter integer := 20;
     iter_cnt integer := 0;
 BEGIN
+    CREATE TEMPORARY TABLE isbelastmet
+    (
+        id                           int,
+        rust_op_kadastraalobject_id  int,
+        rust_op_kadastraalobj_volgnr int,
+        kot_identificatie            varchar,
+        kot_status_code              varchar,
+        zrt_begindatum               timestamp,
+        zrt_einddatum                timestamp,
+        expiration_date              timestamp,
+        teostandsdatum               timestamp,
+        creation                     timestamp,
+        modification                 timestamp
+    );
+
     LOOP
+        INSERT INTO isbelastmet
+        SELECT zrtbelastmet.id,
+               zrtkot.rust_op_kadastraalobject_id,
+               zrtkot.rust_op_kadastraalobj_volgnr,
+               kot.brk_kot_id      AS kot_identificatie,
+               kot.status_code     AS kot_status_code,
+               kot.creation        AS zrt_begindatum,
+               kot.einddatum       AS zrt_einddatum,
+               kot.expiration_date AS expiration_date,
+               kot.toestandsdatum  AS toestandsdatum,
+               kot.creation        AS creation,
+               kot.modification    AS modification
+        FROM brk.zakelijkrecht_isbelastmet bel
+                 LEFT JOIN brk_prep.zrt_kot_wip zrtkot
+                           ON zrtkot.id = zakelijkrecht_id
+                 LEFT JOIN brk_prep.zrt_kot_wip zrtbelastmet
+                           ON zrtbelastmet.id = bel.is_belast_met
+                 LEFT JOIN brk_prep.kadastraal_object kot
+                           ON kot.nrn_kot_id = zrtkot.rust_op_kadastraalobject_id
+                               AND kot.nrn_kot_volgnr = zrtkot.rust_op_kadastraalobj_volgnr
+        WHERE zrtkot.rust_op_kadastraalobject_id IS NOT NULL
+          AND zrtbelastmet.rust_op_kadastraalobject_id IS NULL;
+
         UPDATE brk_prep.zrt_kot_wip zrt
         SET rust_op_kadastraalobject_id=kot_id,
             rust_op_kadastraalobj_volgnr=kot_volgnr,
@@ -41,35 +80,17 @@ BEGIN
             toestandsdatum=v.toestandsdatum,
             creation=v.creation,
             modification=v.modification
-        FROM (SELECT zrtbelastmet.id,
-                     zrtkot.rust_op_kadastraalobject_id,
-                     zrtkot.rust_op_kadastraalobj_volgnr,
-                     kot.brk_kot_id      AS kot_identificatie,
-                     kot.status_code     AS kot_status_code,
-                     kot.creation        AS zrt_begindatum,
-                     kot.einddatum       AS zrt_einddatum,
-                     kot.expiration_date AS expiration_date,
-                     kot.toestandsdatum  AS toestandsdatum,
-                     kot.creation        AS creation,
-                     kot.modification    AS modification
-              FROM brk.zakelijkrecht_isbelastmet bel
-                       LEFT JOIN brk_prep.zrt_kot_wip zrtkot
-                                 ON zrtkot.id = zakelijkrecht_id
-                       LEFT JOIN brk_prep.zrt_kot_wip zrtbelastmet
-                                 ON zrtbelastmet.id = bel.is_belast_met
-                       LEFT JOIN brk_prep.kadastraal_object kot
-                                 ON kot.nrn_kot_id = zrtkot.rust_op_kadastraalobject_id
-                                     AND kot.nrn_kot_volgnr = zrtkot.rust_op_kadastraalobj_volgnr
-              WHERE zrtkot.rust_op_kadastraalobject_id IS NOT NULL
-                AND zrtbelastmet.rust_op_kadastraalobject_id IS NULL) AS v(id, kot_id, kot_volgnr, kot_identificatie,
-                                                                           kot_status_code, begindatum, einddatum,
-                                                                           expiration_date, toestandsdatum, creation,
-                                                                           modification)
+        FROM isbelastmet AS v(id, kot_id, kot_volgnr, kot_identificatie,
+                              kot_status_code, begindatum, einddatum,
+                              expiration_date, toestandsdatum, creation,
+                              modification)
         WHERE v.id = zrt.id;
 
         GET DIAGNOSTICS lastres = ROW_COUNT;
         total := total + lastres;
         iter_cnt := iter_cnt + 1;
+
+        TRUNCATE isbelastmet;
 
         EXIT WHEN lastres = 0 OR iter_cnt > max_iter;
     END LOOP;
--- src/data/sql/brk2/kot.remove_duplicate_ontstaan_uit_relations.sql
+++ src/data/sql/brk2/kot.remove_duplicate_ontstaan_uit_relations.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION brk2_undouble_g_perceel_relations() RETURNS integer A
 $$
     -- Removes duplicate relations from is_ontstaan_uit_brk_g_perceel and is_ontstaan_uit_brk_kadastraalobject columns.
 -- JSON objects in those columns contain (id, volgnummer, identificatie) combinations because they are
--- needed in kot.update_a_geometrie.sql. We remove the id and volgnummer attributes from the JSON and undouble
+-- needed in preselect.kot_geo.sql. We remove the id and volgnummer attributes from the JSON and undouble
 -- the resulting list of objects with only the 'identificatie' attribute.
 DECLARE
     batch_size integer := 10000;
@@ -11,67 +11,89 @@ DECLARE
     total      integer := 0;
     lastres    integer := 0;
 BEGIN
-    SELECT max(id) FROM brk2_prep.kadastraal_object INTO max_id;
+    SELECT MAX(id) FROM brk2_prep.kadastraal_object INTO max_id;
+    CREATE TABLE brk2_prep.ontstaan_uit_g_perceel
+    (
+        id           int,
+        volgnummer   int,
+        new_relation jsonb
+    );
+    CREATE TABLE brk2_prep.ontstaan_uit_kadastraalobject
+    (
+        id           int,
+        volgnummer   int,
+        new_relation jsonb
+    );
+
     LOOP
         -- Undouble g_perceel
-        UPDATE brk2_prep.kadastraal_object kot
-        SET is_ontstaan_uit_brk_g_perceel=new_relations.new_relation
-        FROM (SELECT id,
-                     volgnummer,
-                     array_to_json(
-                             array_agg(
-                                     json_build_object(
-                                             'kot_identificatie', kot_identificatie
-                                         )
-                                 )
-                         ) as new_relation
-              FROM (SELECT kot.id,
-                           kot.volgnummer,
-                           gperc ->> 'kot_identificatie' AS kot_identificatie
-                    FROM brk2_prep.kadastraal_object kot
-                             JOIN jsonb_array_elements(kot.is_ontstaan_uit_brk_g_perceel) gperc ON TRUE
-                    WHERE kot.is_ontstaan_uit_brk_g_perceel IS NOT NULL
-                      AND kot.id >= current_id
-                      AND kot.id < (current_id + batch_size)
-                    GROUP BY kot.id, kot.volgnummer, gperc ->> 'kot_identificatie') q
-              GROUP BY id, volgnummer) new_relations
-        WHERE new_relations.id = kot.id
-          AND new_relations.volgnummer = kot.volgnummer;
-
-        GET DIAGNOSTICS lastres = ROW_COUNT;
-        total = total + lastres;
+        INSERT INTO brk2_prep.ontstaan_uit_g_perceel
+        SELECT id,
+               volgnummer,
+               ARRAY_TO_JSON(
+                       ARRAY_AGG(
+                               JSON_BUILD_OBJECT(
+                                       'kot_identificatie', kot_identificatie
+                                   )
+                           )
+                   ) AS new_relation
+        FROM (SELECT kot.id,
+                     kot.volgnummer,
+                     gperc ->> 'kot_identificatie' AS kot_identificatie
+              FROM brk2_prep.kadastraal_object kot
+                       JOIN JSONB_ARRAY_ELEMENTS(kot.is_ontstaan_uit_brk_g_perceel) gperc ON TRUE
+              WHERE kot.is_ontstaan_uit_brk_g_perceel IS NOT NULL
+                AND kot.id >= current_id
+                AND kot.id < (current_id + batch_size)
+              GROUP BY kot.id, kot.volgnummer, gperc ->> 'kot_identificatie') q
+        GROUP BY id, volgnummer;
 
         -- Undouble ontstaan_uit_kadastraalobject
-        UPDATE brk2_prep.kadastraal_object kot
-        SET is_ontstaan_uit_brk_kadastraalobject=new_relations.new_relation
-        FROM (SELECT id,
-                     volgnummer,
-                     array_to_json(
-                             array_agg(
-                                     json_build_object(
-                                             'kot_identificatie', kot_identificatie
-                                         )
-                                 )
-                         ) as new_relation
-              FROM (SELECT kot.id,
-                           kot.volgnummer,
-                           gperc ->> 'kot_identificatie' AS kot_identificatie
-                    FROM brk2_prep.kadastraal_object kot
-                             JOIN jsonb_array_elements(kot.is_ontstaan_uit_brk_kadastraalobject) gperc ON TRUE
-                    WHERE kot.is_ontstaan_uit_brk_kadastraalobject IS NOT NULL
-                      AND kot.id >= current_id
-                      AND kot.id < (current_id + batch_size)
-                    GROUP BY kot.id, kot.volgnummer, gperc ->> 'kot_identificatie') q
-              GROUP BY id, volgnummer) new_relations
-        WHERE new_relations.id = kot.id
-          AND new_relations.volgnummer = kot.volgnummer;
-
-        GET DIAGNOSTICS lastres = ROW_COUNT;
-        total = total + lastres;
+        INSERT INTO brk2_prep.ontstaan_uit_kadastraalobject
+        SELECT id,
+               volgnummer,
+               ARRAY_TO_JSON(
+                       ARRAY_AGG(
+                               JSON_BUILD_OBJECT(
+                                       'kot_identificatie', kot_identificatie
+                                   )
+                           )
+                   ) AS new_relation
+        FROM (SELECT kot.id,
+                     kot.volgnummer,
+                     gperc ->> 'kot_identificatie' AS kot_identificatie
+              FROM brk2_prep.kadastraal_object kot
+                       JOIN JSONB_ARRAY_ELEMENTS(kot.is_ontstaan_uit_brk_kadastraalobject) gperc ON TRUE
+              WHERE kot.is_ontstaan_uit_brk_kadastraalobject IS NOT NULL
+                AND kot.id >= current_id
+                AND kot.id < (current_id + batch_size)
+              GROUP BY kot.id, kot.volgnummer, gperc ->> 'kot_identificatie') q
+        GROUP BY id, volgnummer;
 
         current_id = current_id + batch_size;
         EXIT WHEN current_id > max_id;
     END LOOP;
+
+    -- Update g_perceel relations
+    UPDATE brk2_prep.kadastraal_object kot
+    SET is_ontstaan_uit_brk_g_perceel=t.new_relation
+    FROM brk2_prep.ontstaan_uit_g_perceel t
+    WHERE t.id = kot.id
+      AND t.volgnummer = kot.volgnummer;
+
+    GET DIAGNOSTICS lastres = ROW_COUNT;
+    total = total + lastres;
+
+    -- Update ontstaan-uit_kadastraalobject relations
+    UPDATE brk2_prep.kadastraal_object kot
+    SET is_ontstaan_uit_brk_kadastraalobject=t.new_relation
+    FROM brk2_prep.ontstaan_uit_kadastraalobject t
+    WHERE t.id = kot.id
+      AND t.volgnummer = kot.volgnummer;
+
+    GET DIAGNOSTICS lastres = ROW_COUNT;
+    total = total + lastres;
+
     RETURN total;
 
 END;
