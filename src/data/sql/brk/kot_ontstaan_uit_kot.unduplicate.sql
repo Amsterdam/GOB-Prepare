e7350c6f192cb98602bde717277cc9ff33b29a79
JKoetsier <jaap@chariot-it.nl>
2023-02-14 13:52:35 +0100

    Fix unduplicate queries; add aliases and fix wrongly referenced column name for ontstaan_uit_kot (#434)

--- src/data/sql/brk/kot_ontstaan_uit_kot.unduplicate.sql
+++ src/data/sql/brk/kot_ontstaan_uit_kot.unduplicate.sql
@@ -8,13 +8,13 @@ SELECT nrn_kot_id,
                JSONB_BUILD_OBJECT(
                        'brk_kot_id', brk_kot_id
                    ) ORDER BY brk_kot_id
-           )
+           ) AS ontstaan_uit_kadastraalobject
 FROM (SELECT kot.nrn_kot_id,
              kot.nrn_kot_volgnr,
              gperc ->> 'brk_kot_id' AS brk_kot_id
       FROM brk_prep.kot_ontstaan_uit_kot_doubles kot
-               LEFT JOIN JSONB_ARRAY_ELEMENTS(kot.relatie_g_perceel) gperc ON TRUE
-      WHERE kot.relatie_g_perceel IS NOT NULL
+               LEFT JOIN JSONB_ARRAY_ELEMENTS(kot.ontstaan_uit_kadastraalobject) gperc ON TRUE
+      WHERE kot.ontstaan_uit_kadastraalobject IS NOT NULL
       GROUP BY kot.nrn_kot_id, kot.nrn_kot_volgnr, gperc ->> 'brk_kot_id') q
 GROUP BY nrn_kot_id, nrn_kot_volgnr
 ;
