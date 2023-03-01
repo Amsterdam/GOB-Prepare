f7af13af8029be06af450e2b0d38ae4bc6bd022a
Jaap Koetsier <jaap@chariot-it.nl>
2023-03-01 10:28:07 +0000

    Ignore KOT's with status 'H' and exclude 'status' column

--- src/data/sql/brk2/preselect.kot.geldigheid.sql
+++ src/data/sql/brk2/preselect.kot.geldigheid.sql
@@ -1,12 +1,10 @@
 SELECT kot.id,
        kot.volgnummer,
        kot.toestandsdatum AS begin_geldigheid,
-       CASE
-           WHEN kot.status_code = 'H' THEN mte.datum
-           ELSE LEAD(kot.toestandsdatum, 1) OVER (
-               PARTITION BY kot.identificatie
-               ORDER BY kot.volgnummer
-               )
-           END            AS eind_geldigheid
+       LEAD(kot.toestandsdatum, 1) OVER (
+           PARTITION BY kot.identificatie
+           ORDER BY kot.volgnummer
+           )              AS eind_geldigheid
 FROM brk2.kadastraal_object kot
          LEFT JOIN brk2.mutatie mte ON kot.mutatie_id = mte.id
+WHERE kot.status_code = 'B'
