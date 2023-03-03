dbb9930315d1b83c1a9f0f7b4e5e0617d7bb2ea4
JKoetsier <jaap@chariot-it.nl>
2023-03-03 13:52:33 +0000

    Move kot status selection to other query to prevent weird Postgres pa… (#451)
    …rallel error

--- src/data/sql/brk2/preselect.zrt_kot.sql
+++ src/data/sql/brk2/preselect.zrt_kot.sql
@@ -1,3 +1,3 @@
 -- Copies the zrt_kot_wip table. The zrt_kot_wip table is a default Postgres table because of all the update
 -- actions involved. This query copies that table into a columnar table, if citus is available.
-SELECT * FROM brk2_prep.zrt_kot_wip WHERE __kot_status = 'B';
+SELECT * FROM brk2_prep.zrt_kot_wip;
