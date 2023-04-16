8b0468bf47f19afbf36285bb1aceb5538567d5c8
Peter Adrichem <Peter.Adrichem@gmail.com>
2023-04-16 14:23:30 +0200

    Code checks for gobprepare.__main__ and gobprepare.prepare_client.

--- src/gobprepare/typing.py
+++ src/gobprepare/typing.py
@@ -1,6 +1,7 @@
 """GOB Prepare typing."""
 
 
+from collections.abc import Collection
 from typing import Any, Literal, TypedDict
 
 
@@ -16,6 +17,46 @@ class PrepareMapping(TypedDict):
     version: str
 
 
+class TaskOverride(TypedDict, total=False):
+    """Task Override."""
+
+    ignore: list[str]
+    include: list[str]
+    type: str
+
+
+Summary = dict[str, Collection[str]]
+
+
+class Message(TypedDict, total=False):
+    """Message."""
+
+    contents: dict[str, str]
+    header: dict[str, Any]
+    original_action: str
+    override: TaskOverride
+    summary: Summary
+
+
+class Task(TypedDict, total=False):
+    """Task configuration."""
+
+    task_name: str
+    dependencies: list[str]
+    extra_msg: Message
+
+
+TaskList = list[Task]
+
+
+class ClearConfig(TypedDict):
+    """Clear action configuration."""
+
+    id: Literal["clear_schemas"]
+    schemas: list[str]
+    type: Literal["clear"]
+
+
 class ActionCommonConfig(TypedDict):
     """Action base configuration."""
 
@@ -29,3 +70,25 @@ class SQLBaseConfig(ActionCommonConfig):
     description: str
     query_src: Literal["file", "string"]
     query: str
+
+
+class ExecuteSQLConfig(SQLBaseConfig):
+    """Execute SQL action configuration."""
+
+    type: Literal["execute_sql"]
+
+
+class CreateTableConfig(SQLBaseConfig):
+    """Create Table action configuration."""
+
+    table_name: str
+    type: Literal["create_table"]
+
+
+class PublishSchemasConfig(TypedDict):
+    """Publish Schemas action configuration."""
+
+    depends_on: Literal["*"]
+    id: str
+    publish_schemas: dict[str, str]
+    type: Literal["publish_schemas"]
--- src/test.sh
+++ src/test.sh
@@ -9,54 +9,23 @@ echo() {
 
 export COVERAGE_FILE="/tmp/.coverage"
 
-# Uncomment files to pass through checks
-FILES=(
-  "gobprepare/cloner/mapping/__init__.py"
-  "gobprepare/cloner/mapping/oracle_to_postgres.py"
-  "gobprepare/cloner/__init__.py"
-  "gobprepare/cloner/oracle_to_postgres.py"
-  "gobprepare/cloner/typing.py"
-  "gobprepare/selector/_selector.py"
-  "gobprepare/selector/__init__.py"
-  "gobprepare/selector/_from_datastore.py"
-  "gobprepare/selector/_to_postgres.py"
-  "gobprepare/selector/datastore_to_postgres.py"
-  "gobprepare/selector/typing.py"
-  "gobprepare/config.py"
-  "gobprepare/importers/__init__.py"
-  "gobprepare/importers/api_importer.py"
-  "gobprepare/importers/csv_importer.py"
-  "gobprepare/importers/typing.py"
-  "gobprepare/__init__.py"
-  "gobprepare/utils/graphql.py"
-  "gobprepare/utils/__init__.py"
-  "gobprepare/utils/postgres.py"
-  "gobprepare/utils/exceptions.py"
-  "gobprepare/utils/sql.py"
-  "gobprepare/utils/requests.py"
-  "gobprepare/utils/typing.py"
-  "gobprepare/mapping.py"
-#  "gobprepare/prepare_client.py"
-  "gobprepare/typing.py"
-#  "gobprepare/__main__.py"
-)
 
 echo "Running mypy"
-mypy "${FILES[@]}"
+mypy gobprepare
 
 echo "\nRunning unit tests"
 coverage run --source=gobprepare -m pytest
 
-echo "Coverage report"
+echo "\nCoverage report"
 coverage report --fail-under=100
 
 echo "\nCheck if Black finds potential reformat fixes"
-black --check --diff "${FILES[@]}"
+black --check --diff gobprepare
 
 echo "\nCheck for potential import sort"
-isort --check --diff --src-path=gobprepare "${FILES[@]}"
+isort --check --diff --src-path=gobprepare gobprepare
 
 echo "\nRunning Flake8 style checks"
-flake8 "${FILES[@]}"
+flake8 gobprepare
 
 echo "\nChecks complete"
