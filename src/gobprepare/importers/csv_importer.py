8c7b8bfff2bdfa1f8e55853808715ea0c96662c3
Roel de Vries <77724088+roel-devries@users.noreply.github.com>
2023-06-05 14:40:58 +0200

    Use iterator during CSV import to prevent oom (#513)
    Problem: A 2gb csv file causes OOM, because its copied during
    processing.
    
    - Use chunks when reading csv files
    - refactor `_load_from_objectstore` from import csv class and use as
    contextmanager
    - refactor `retry` function to request.utils 
    - Remove temporary file after import
    - Use Postgres' `TEXT` type instead of `VARCHAR` to omit calculating max
    length of column values
    - `ANALYZE` table after import

--- src/gobprepare/importers/csv_importer.py
+++ src/gobprepare/importers/csv_importer.py
@@ -1,34 +1,44 @@
-import http.client
-import os
+import contextlib
 import tempfile
-import time
-from typing import Optional, TypedDict
-from urllib.error import HTTPError
+from typing import Iterator, Optional
 
+import pandas as pd
 from gobconfig.datastore.config import get_datastore_config
 from gobcore.datastore.factory import DatastoreFactory
 from gobcore.datastore.objectstore import ObjectDatastore
 from gobcore.datastore.sql import SqlDatastore
 from gobcore.exceptions import GOBException
+from pandas import NA as pd_NA
 from pandas import read_csv
 from pandas.errors import ParserError
+from pandas.io.parsers import TextFileReader
 
 from gobprepare.importers.typing import ReadConfig, SqlCsvImporterConfig
 from gobprepare.utils.postgres import create_table_columnar_query
+from gobprepare.utils.requests import retry
 
 
-class CSVColumn(TypedDict):
-    """CSV column."""
+@contextlib.contextmanager
+def _load_from_objectstore(datastore: ObjectDatastore) -> Iterator[str]:
+    datastore.connect()
 
-    max_length: int
-    name: str
+    obj_info = next(datastore.query(None), None)
 
+    if obj_info is None:
+        raise GOBException(f"File not found on Objectstore: {datastore.read_config['file_filter']}")
 
-class CSVData(TypedDict):
-    """CSV data."""
+    _, obj = datastore.connection.get_object(
+        container=datastore.container_name, obj=obj_info["name"], resp_chunk_size=100_000_000
+    )
 
-    columns: list[CSVColumn]
-    data: list[list[Optional[str]]]
+    with tempfile.NamedTemporaryFile(mode="wb", suffix=".csv") as fp:
+        try:
+            for chunk in obj:
+                fp.write(chunk)
+            print(f"File loaded from objectstore: {obj_info['name']}")
+            yield fp.name
+        finally:
+            datastore.disconnect()
 
 
 class SqlCsvImporter:
@@ -37,32 +47,24 @@ class SqlCsvImporter:
     MAX_RETRIES = 5
     WAIT_RETRY = 2
 
-    def __init__(self, dst_datastore: SqlDatastore, config: SqlCsvImporterConfig) -> None:
+    CSV_CHUNK_SIZE = 100_000
+
+    def __init__(self, dst_datastore: SqlDatastore, config: SqlCsvImporterConfig):
         """Initialise SqlCsvImporter."""
         self._dst_datastore = dst_datastore
         self._destination = config["destination"]
 
-        if config.get("objectstore"):
-            self._source = self._load_from_objectstore(config["objectstore"], config["read_config"])
-        elif config.get("source"):
-            self._source = config["source"]
-        else:
-            raise GOBException("Incomplete config. Expecting key 'objectstore' or 'source'")
+        self._read_config: Optional[ReadConfig] = config.get("read_config")
+        self._objectstore: Optional[str] = config.get("objectstore")
+        self._source: Optional[str] = config.get("source")
 
         # Mapping of CSV columns to database columns (default CSV columns will be used if no alternative supplied)
         self._column_names = config.get("column_names", {})
         self._separator = config.get("separator", ",")
         self._encoding = config.get("encoding", "utf-8")
 
-    def _is_empty_row(self, row) -> bool:
-        """Return True if pandas row is empty.
-
-        :param row:
-        :return:
-        """
-        return all([True if i is None or not str(i) else False for i in row.tolist()])
-
-    def _load_csv(self) -> CSVData:
+    @retry(MAX_RETRIES, WAIT_RETRY)
+    def _load_csv_chunk(self, reader: TextFileReader, src_path: str) -> Optional[pd.DataFrame]:
         """Load CSV with data.
 
         Download over HTTP if necessary.
@@ -70,87 +72,83 @@ class SqlCsvImporter:
 
         :return:
         """
-        tries = 0
-        while True:
-            try:
-                df = read_csv(
-                    self._source, keep_default_na=False, sep=self._separator, dtype=str, encoding=self._encoding
-                )
-            except ParserError:
-                raise GOBException(f"Can't parse CSV: {self._source}")
-            except (HTTPError, http.client.HTTPException):
-                tries += 1
-                if tries >= self.MAX_RETRIES:
-                    raise GOBException(f"Problems downloading CSV: {self._source}")
-
-                time.sleep(self.WAIT_RETRY)
-            else:
-                break  # pragma: no cover
-
-        return {
-            "columns": [
-                {
-                    "max_length": max([len(str(i)) for i in df[col]]),
-                    "name": self._column_names.get(col, col),
-                }
-                for col in df.columns
-            ],
-            # List of lists of values
-            "data": [
-                list(map(lambda x: str(x) if x else None, row.tolist()))
-                for _, row in df.iterrows()
-                if not self._is_empty_row(row)
-            ],
-        }
-
-    def _tmp_filename(self, filename: str) -> str:
-        new_location = os.path.join(tempfile.gettempdir(), filename)
-        os.makedirs(os.path.dirname(new_location), exist_ok=True)
-        return new_location
-
-    def _load_from_objectstore(self, objectstore: str, read_config: ReadConfig) -> str:
-        objectstore = DatastoreFactory.get_datastore(get_datastore_config(objectstore), read_config)
-        assert isinstance(objectstore, ObjectDatastore), "Expected Objectstore"
-
-        objectstore.connect()
-
         try:
-            obj_info = next(objectstore.query(None))
+            return reader.get_chunk(self.CSV_CHUNK_SIZE)
         except StopIteration:
-            raise GOBException(f"File not found on Objectstore: {read_config['file_filter']}")
+            return None
+        except ParserError:
+            raise GOBException(f"CSV parsing exception: {src_path}")
 
-        new_location = self._tmp_filename(obj_info["name"])
-        obj = objectstore.connection.get_object(objectstore.container_name, obj_info["name"])[1]
+    def _process_chunk(self, df: pd.DataFrame) -> tuple[list[str], list[tuple[Optional[str]]]]:
+        # Drop empty rows
+        df.dropna(axis="index", how="all", inplace=True)
+        # Replace empty values with None to insert NULL in database
+        df.replace(to_replace=pd_NA, value=None, inplace=True)
 
-        with open(new_location, "wb") as fp:
-            fp.write(obj)
-        return new_location
+        columns = [self._column_names.get(col, col) for col in df if isinstance(col, str)]
+        data = [row for row in zip(*[df[col] for col in df])]
 
-    def _create_destination_table(self, columns: list[CSVColumn]) -> None:
+        return columns, data
+
+    def _create_destination_table(self, columns: list[str]):
         """Create a destination table.
 
         :param columns:
         :return:
         """
-        # This works for Postgres and probably for most SQL databases
-        columndefs = ",".join([f"\"{col['name']}\" VARCHAR({col['max_length'] + 5}) NULL" for col in columns])
+        # Destination is PostgreSQL, which supports TEXT
+        columndefs = ", ".join([f'"{col}" TEXT NULL' for col in columns])
         query = create_table_columnar_query(self._dst_datastore, self._destination, columndefs)
         self._dst_datastore.execute(query)
 
-    def _import_data(self, data: list[list[Optional[str]]]) -> None:
+    def _import_data(self, data: list[tuple[Optional[str]]]) -> int:
         """Write CSV data to destination table.
 
         :param data:
         :return:
         """
-        self._dst_datastore.write_rows(self._destination, data)
+        return self._dst_datastore.write_rows(self._destination, data)  # type: ignore
+
+    def _import_csv(self, source_path: str) -> int:
+        inserted_rows = 0
+
+        with read_csv(
+            source_path,
+            index_col=False,
+            keep_default_na=False,
+            na_values="",  # only convert empty strings to NaN
+            sep=self._separator,
+            dtype=str,  # force string dtypes
+            encoding=self._encoding,
+            iterator=True,
+        ) as reader:
+            while (chunk := self._load_csv_chunk(reader, source_path)) is not None:
+                columns, data = self._process_chunk(chunk)
+
+                if not inserted_rows:
+                    self._create_destination_table(columns)
+
+                inserted_rows += self._import_data(data)
+
+        if inserted_rows > 0:
+            self._dst_datastore.execute(f"ANALYZE {self._destination}")
+
+        return inserted_rows
 
     def import_csv(self) -> int:
         """Entry method. Return number of inserted rows.
 
         :return:
         """
-        data = self._load_csv()
-        self._create_destination_table(data["columns"])
-        self._import_data(data["data"])
-        return len(data["data"])
+        if self._objectstore and not self._source:
+            datastore = DatastoreFactory.get_datastore(get_datastore_config(self._objectstore), self._read_config)
+
+            if not isinstance(datastore, ObjectDatastore):
+                raise GOBException(f"Expected objectstore, got: {type(datastore)}")
+
+            with _load_from_objectstore(datastore) as src_file:
+                return self._import_csv(src_file)
+        elif not self._objectstore and self._source:
+            return self._import_csv(self._source)
+        else:
+            raise GOBException("Incomplete config. Expecting key 'objectstore' or 'source'")
