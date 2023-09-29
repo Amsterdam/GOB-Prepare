88cd6b25f6676ec1944569f9c078023df42ac337
JKoetsier <jaap@chariot-it.nl>
2023-09-29 10:00:33 +0200

    92449: Remove 'einddatum_recht' from calculations of expiration_date/datum_actueel_tot/eind_geldigheid for akt and tng (#587)

--- src/data/sql/brk2/select.aantekening_kadastraal_object.sql
+++ src/data/sql/brk2/select.aantekening_kadastraal_object.sql
@@ -1,21 +1,21 @@
-SELECT atg.identificatie                                               AS identificatie,
-       atg.id                                                          AS __neuron_id,
-       idc.ident_oud                                                   AS was_identificatie,
-       koa.kadastraalobject_volgnummer                                 AS volgnummer,
-       kot.begin_geldigheid                                            AS begin_geldigheid,
-       LEAST(kot._expiration_date, atg.einddatum, atg.einddatum_recht) AS eind_geldigheid,
-       atg.einddatum_recht                                             AS einddatum_recht,
-       atg.aardaantekening_code                                        AS aard_code,
-       aag.omschrijving                                                AS aard_omschrijving,
-       atg.omschrijving                                                AS omschrijving,
-       atg.betreft_gedeelte_van_perceel                                AS betreft_gedeelte_van_perceel,
-       abn.sjt_identificaties                                          AS heeft_brk_betrokken_persoon,
-       kot.identificatie                                               AS heeft_betrekking_op_brk_kadastraal_object,
-       atg.stukdeel_identificatie                                      AS is_gebaseerd_op_brk_stukdeel,
-       atg.einddatum                                                   AS einddatum,
-       LEAST(kot._expiration_date, atg.einddatum, atg.einddatum_recht) AS datum_actueel_tot,
-       LEAST(kot._expiration_date, atg.einddatum, atg.einddatum_recht) AS _expiration_date,
-       kot.toestandsdatum                                              AS toestandsdatum
+SELECT atg.identificatie                          AS identificatie,
+       atg.id                                     AS __neuron_id,
+       idc.ident_oud                              AS was_identificatie,
+       koa.kadastraalobject_volgnummer            AS volgnummer,
+       kot.begin_geldigheid                       AS begin_geldigheid,
+       LEAST(kot._expiration_date, atg.einddatum) AS eind_geldigheid,
+       atg.einddatum_recht                        AS einddatum_recht,
+       atg.aardaantekening_code                   AS aard_code,
+       aag.omschrijving                           AS aard_omschrijving,
+       atg.omschrijving                           AS omschrijving,
+       atg.betreft_gedeelte_van_perceel           AS betreft_gedeelte_van_perceel,
+       abn.sjt_identificaties                     AS heeft_brk_betrokken_persoon,
+       kot.identificatie                          AS heeft_betrekking_op_brk_kadastraal_object,
+       atg.stukdeel_identificatie                 AS is_gebaseerd_op_brk_stukdeel,
+       atg.einddatum                              AS einddatum,
+       LEAST(kot._expiration_date, atg.einddatum) AS datum_actueel_tot,
+       LEAST(kot._expiration_date, atg.einddatum) AS _expiration_date,
+       kot.toestandsdatum                         AS toestandsdatum
 FROM brk2.aantekening atg
          JOIN brk2.kadastraal_object_aantekening koa ON koa.aantekening_identificatie = atg.identificatie
     -- Filter all aantekeningen based on aardaantekening != Aantekening PB
@@ -23,8 +23,8 @@ FROM brk2.aantekening atg
          JOIN (SELECT * FROM brk2.import_aardaantekening aag WHERE aag.type != 'Aantekening PB') aag
               ON atg.aardaantekening_code = aag.code
          JOIN brk2_prep.kadastraal_object kot
-                   ON kot.id = koa.kadastraalobject_id AND
-                      kot.volgnummer = koa.kadastraalobject_volgnummer
+              ON kot.id = koa.kadastraalobject_id AND
+                 kot.volgnummer = koa.kadastraalobject_volgnummer
          LEFT JOIN (SELECT abn.aantekening_id,
                            JSONB_AGG(
                                    JSONB_BUILD_OBJECT(
