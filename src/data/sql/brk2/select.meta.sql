2fb0e9a9d64ffc817f2bff45affcda2597cd734b
Roel de Vries <roel.de.vries@amsterdam.nl>
2023-10-23 15:18:16 +0200

    Update meta omschrijving

--- src/data/sql/brk2/select.meta.sql
+++ src/data/sql/brk2/select.meta.sql
@@ -1,5 +1,5 @@
-SELECT 1                                                                                                               AS id,
-       DATE_TRUNC('DAY', MAX(bsd.datum_aangemaakt))                                                                    AS toestandsdatum,
-       'Toestandsdatum, d.i. de laatste aanmaakdatum van de BRK-berichten in de naar DIVA gerepliceerde BRK-levering.' AS omschrijving
+SELECT 1                                                                                                        AS id,
+       DATE_TRUNC('DAY', MAX(bsd.datum_aangemaakt))                                                             AS toestandsdatum,
+       'Toestandsdatum, d.i. de laatste aanmaakdatum van de BRK-berichten in de laatst verwerkte BRK-levering.' AS omschrijving
 FROM brk2.bestand bsd
 ;
