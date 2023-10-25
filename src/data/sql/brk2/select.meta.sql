9eebb4fdff9de5d9e75db52ba62a6ec401130f57
Roel de Vries <roel.de.vries@amsterdam.nl>
2023-10-25 13:18:21 +0200

    Omit columnar in create table

--- src/data/sql/brk2/select.meta.sql
+++ src/data/sql/brk2/select.meta.sql
@@ -1,3 +1,4 @@
+CREATE TABLE brk2_prep.meta AS
 SELECT 1                                                                                                        AS id,
        DATE_TRUNC('DAY', MAX(bsd.datum_aangemaakt))                                                             AS toestandsdatum,
        'Toestandsdatum, d.i. de laatste aanmaakdatum van de BRK-berichten in de laatst verwerkte BRK-levering.' AS omschrijving
